d6010e857e7bca62f26360bc3cb82e7396907d43
monnaagii psc net <monnaagii@gmail.com>
2021-03-29 10:46:44 +0800

    first commit

---
+++ DemoProgram/LoginForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoProgram
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            //Энд нэр нууц үгээ шалгаад үнэн бол доорхи мөрийг ажиллуулна.
+            DialogResult = DialogResult.OK;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            //Программыг хаахдаа ингэж хаах нь зүйтэй
+            Application.Exit();
+        }
+    }
+}
---
+++ DemoProgram/MainForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoProgram
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            LoginForm frmLogin = new LoginForm();
+            if (frmLogin.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("Ok");
+            } else {
+                MessageBox.Show("Cancel");
+            }
+        }
+    }
+}
